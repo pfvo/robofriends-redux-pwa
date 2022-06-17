d222e3426ad0feb04580bb3c80e976734c679a39
pfvo <pedrofvoliveira94@gmail.com>
2022-06-17 02:13:15 +0100

    committing pwa files

--- src/index.js
+++ src/index.js
@@ -25,4 +25,9 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
